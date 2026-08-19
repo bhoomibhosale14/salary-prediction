86ba4ab9a67648330e3fa31961a1cd7b800dc0ff
bhoomibhosale14 <bhoomibhosale1@gmail.com>
2026-08-19 11:11:19 +0530

    Fix syntax error in job_title selection

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -20,7 +20,7 @@ st.title("Salary Prediction")
 age = st.number_input("Age",18,65)
 gender = st.selectbox("Gender",encoder["Gender"].classes_)
 education_level = st.selectbox("Education level",encoder["Education level"].classes_)
-job_title = st.selectbox("Job Title",encoder=["Job Title"].classes_)
+job_title = st.selectbox("Job Title",encoder["Job Title"].classes_)
 years_of_exp = st.number_input("Years of Experience",0,50)
 
 df = pd.DataFrame({
