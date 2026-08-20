aa477ca4863fb4016d594146f87ab64f005d216c
bhoomibhosale14 <bhoomibhosale1@gmail.com>
2026-08-20 07:39:03 +0530

    Update salary_prediction_deployment.py

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -19,7 +19,7 @@ st.title("Salary Prediction")
 
 age = st.number_input("Age",18,65)
 gender = st.selectbox("Gender",encoder["Gender"].classes_)
-education_level = st.selectbox("Education level",encoder["Education level"].classes_)
+education_level = st.selectbox("Education level",encoder["Education Level"].classes_)
 job_title = st.selectbox("Job Title",encoder["Job Title"].classes_)
 years_of_exp = st.number_input("Years of Experience",0,50)
 
